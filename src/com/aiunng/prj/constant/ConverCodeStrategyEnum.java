82301c349939e8cf83f2437b00de26b2a6b1e35b
wangxinyu <wangxinyu@flashexpress.com>
2022-04-26 11:01:34 +0800

    morse编码解码

--- src/com/aiunng/prj/constant/ConverCodeStrategyEnum.java
+++ src/com/aiunng/prj/constant/ConverCodeStrategyEnum.java
@@ -1,6 +1,7 @@
 package com.aiunng.prj.constant;
 
-import com.aiunng.prj.util.StringUnicodeUtil;
+import cn.hutool.core.codec.Morse;
+import cn.hutool.core.text.UnicodeUtil;
 import com.intellij.openapi.util.text.StringUtil;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
@@ -9,6 +10,7 @@ import java.util.Base64;
 
 /**
  * 转换策略
+ *
  * @author：wangXinYu
  * @date：2021/10/21 6:35 下午
  */
@@ -24,45 +26,59 @@ public enum ConverCodeStrategyEnum {
     }
   },
 
-  URL_ENCODE(201, "url编码") {
+  URL_ENCODE(102, "url编码") {
     @Override
     protected String doConvert(String input) throws UnsupportedEncodingException {
       return URLEncoder.encode(input, "UTF-8");
     }
   },
 
-  URL_DECODE(202, "url解码") {
+  URL_DECODE(103, "url解码") {
     @Override
     protected String doConvert(String input) throws UnsupportedEncodingException {
       return URLDecoder.decode(input, "UTF-8");
     }
   },
 
-  BASE64_ENCODE(301, "base64编码") {
+  BASE64_ENCODE(104, "base64编码") {
     @Override
     protected String doConvert(String input) {
       return Base64.getEncoder().encodeToString(input.getBytes());
     }
   },
 
-  BASE64_DECODE(302, "base64解码") {
+  BASE64_DECODE(105, "base64解码") {
     @Override
     protected String doConvert(String input) {
       return new String(Base64.getDecoder().decode(input));
     }
   },
 
-  UNICODE_ENCODE(401, "unicode编码") {
+  UNICODE_ENCODE(106, "unicode编码") {
     @Override
     protected String doConvert(String input) {
-      return StringUnicodeUtil.string2Unicode(input);
+      return UnicodeUtil.toUnicode(input);
     }
   },
 
-  UNICODE_DECODE(402, "unicode解码") {
+  UNICODE_DECODE(107, "unicode解码") {
     @Override
     protected String doConvert(String input) {
-      return StringUnicodeUtil.unicode2String(input);
+      return UnicodeUtil.toString(input);
+    }
+  },
+
+  MORSE_ENCODE(108, "morse编码") {
+    @Override
+    protected String doConvert(String input) {
+      return new Morse().encode(input);
+    }
+  },
+
+  MORSE_DECODE(109, "morse解码") {
+    @Override
+    protected String doConvert(String input) {
+      return new Morse().decode(input);
     }
   },
 
